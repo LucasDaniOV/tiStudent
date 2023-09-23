1cd6063dfb8fe2a47f579d40cb49f7a58c4e45e1
Lucas Oude Vrielink <lucas@oudevrielink.be>
2023-09-23 23:56:39 +0200

    prettier

--- back-end/domain/model/user.ts
+++ back-end/domain/model/user.ts
@@ -2,12 +2,12 @@ export class User {
     readonly username: string;
     readonly password: string;
 
-    constructor(user: { username: string, password: string }) {
+    constructor(user: { username: string; password: string }) {
         this.username = user.username;
         this.password = user.password;
     }
 
-    equals(otherUser: { username: string, password: string}): boolean {
+    equals(otherUser: { username: string; password: string }): boolean {
         return this.username === otherUser.username && this.password === otherUser.password;
     }
 }
