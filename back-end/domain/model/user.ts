d289942fab4cedd6c570fd2d36be46b36dbb00bd
Lucas Oude Vrielink <lucas@oudevrielink.be>
2023-09-23 23:39:28 +0200

    basic user.ts

---
+++ back-end/domain/model/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    readonly username: string;
+    readonly password: string;
+
+    constructor(user: { username: string, password: string }) {
+        this.username = user.username;
+        this.password = user.password;
+    }
+
+    equals(otherUser: { username: string, password: string}): boolean {
+        return this.username === otherUser.username && this.password === otherUser.password;
+    }
+}
