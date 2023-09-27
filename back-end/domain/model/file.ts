a50a9dd5fe3b5ad300d8297240977e2b2dc351ac
Lucas Oude Vrielink <lucas@oudevrielink.be>
2023-09-27 14:21:12 +0200

    File: remove name property

--- back-end/domain/model/file.ts
+++ back-end/domain/model/file.ts
@@ -1,13 +1,11 @@
 export class File {
-    readonly name: string;
     readonly path: string;
 
-    constructor(file: { name: string; path: string }) {
-        this.name = file.name;
+    constructor(file: { path: string }) {
         this.path = file.path;
     }
 
-    equals(otherFile: { name: string; path: string }): boolean {
-        return this.name === otherFile.name && this.path === otherFile.path;
+    equals(otherFile: { path: string }): boolean {
+        return this.path === otherFile.path;
     }
 }
