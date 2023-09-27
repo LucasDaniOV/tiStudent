60e9e6b9d4d1643b387c1f21e5ec92a99b52b965
Lucas Oude Vrielink <lucas@oudevrielink.be>
2023-09-27 16:12:13 +0200

    Resource: add thumbnail and files properties

--- back-end/domain/model/resource.ts
+++ back-end/domain/model/resource.ts
@@ -7,14 +7,24 @@ export class Resource {
     private updatedAt: Date;
     private title: string;
     private description: string;
+    private thumbnail?: File;
+    private files?: FileList;
 
-    constructor(resource: { creator: User; title: string; description: string }) {
+    constructor(resource: {
+        creator: User;
+        title: string;
+        description: string;
+        thumbnail?: File;
+        files?: FileList;
+    }) {
         const now = new Date();
         this.creator = resource.creator;
         this.createdAt = now;
         this.updatedAt = now;
         this.title = resource.title;
         this.description = resource.description;
+        this.thumbnail = resource.thumbnail;
+        this.files = resource.files;
     }
 
     equals(otherResource: {
@@ -23,13 +33,17 @@ export class Resource {
         updatedAt: Date;
         title: string;
         description: string;
+        thumbnail?: File;
+        files?: FileList;
     }): boolean {
         return (
             this.creator === otherResource.creator &&
             this.createdAt === otherResource.createdAt &&
             this.updatedAt === otherResource.updatedAt &&
             this.title === otherResource.title &&
-            this.description === otherResource.description
+            this.description === otherResource.description &&
+            this.thumbnail === otherResource.thumbnail &&
+            this.files === otherResource.files
         );
     }
 }
