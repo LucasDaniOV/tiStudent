8237cfa55d11de00bc5166a4544c9e6b58a5b095
Lucas Oude Vrielink <lucas@oudevrielink.be>
2023-09-27 14:43:42 +0200

    User: remove username property, add email property

--- back-end/test/domain/model/user.test.ts
+++ back-end/test/domain/model/user.test.ts
@@ -2,13 +2,13 @@ import { User } from '../../../domain/model/user';
 
 test(`given: valid values for user, when: user is created, then: user is created with those values`, () => {
     // given
-    const username = 'test';
+    const email = 'test';
     const password = 'test';
 
     // when
-    const user = new User({ username, password });
+    const user = new User({ email, password });
 
     // then
-    expect(user.username).toEqual(username);
+    expect(user.email).toEqual(email);
     expect(user.password).toEqual(password);
 });
