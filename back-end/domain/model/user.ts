0df5c1b94ffac25c6f6b084f89b6a077885e4d27
DavyBellens <r0882857@ucll.be>
2023-09-28 15:15:47 +0200

    updated user.ts to include optional id number

--- back-end/domain/model/user.ts
+++ back-end/domain/model/user.ts
@@ -1,13 +1,19 @@
 export class User {
+    readonly id?: number;
     readonly email: string;
     readonly password: string;
 
-    constructor(user: { email: string; password: string }) {
+    constructor(user: { id?: number; email: string; password: string }) {
+        this.id = user.id;
         this.email = user.email;
         this.password = user.password;
     }
 
-    equals(otherUser: { email: string; password: string }): boolean {
-        return this.email === otherUser.email && this.password === otherUser.password;
+    equals(otherUser: { id?: number; email: string; password: string }): boolean {
+        return (
+            this.id == otherUser.id &&
+            this.email === otherUser.email &&
+            this.password === otherUser.password
+        );
     }
 }
