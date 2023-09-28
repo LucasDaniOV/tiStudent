816ba1b20a17b85d1e5e9d2c75d9f5b636b37a8f
DavyBellens <r0882857@ucll.be>
2023-09-28 15:17:32 +0200

    created user.db.ts (DAO) with basic code and 2 users

---
+++ back-end/domain/data-access/user.db.ts
@@ -0,0 +1,14 @@
+import { User } from '../model/user';
+
+let currentId = 1;
+
+const users: User[] = [
+    new User({ id: currentId++, email: 'test1@ucll.be', password: 'Password!123' }),
+    new User({ id: currentId++, email: 'test2@ucll.be', password: 'p@ssworD99' }),
+];
+
+const getAllUsers = (): User[] => users;
+
+const getUserById = (id: number): User => users.find((u) => u.id == id);
+
+export default { getAllUsers, getUserById };
