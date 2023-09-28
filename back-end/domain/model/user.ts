bebc3e109dc8ddab78b5085722f2fcd14acd00c6
DavyBellens <r0882857@ucll.be>
2023-09-28 23:17:34 +0200

    validation for creation of user

--- back-end/domain/model/user.ts
+++ back-end/domain/model/user.ts
@@ -5,8 +5,30 @@ export class User {
 
     constructor(user: { id?: number; email: string; password: string }) {
         this.id = user.id;
-        this.email = user.email;
-        this.password = user.password;
+        if (user.email.includes('@')) {
+            this.email = user.email;
+        } else {
+            throw new Error("Email must contain a '@'.");
+        }
+        if (user.password.length >= 8) {
+            if (user.password.match(/\d/)) {
+                if (user.password.match(/[A-Z]/)) {
+                    if (user.password.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/)) {
+                        this.password = user.password;
+                    } else {
+                        throw new Error(
+                            `Password must contain at least 1 special sign (!@#$%^&*()_+-=\[\]{};':"\\|,.<>\/?).`
+                        );
+                    }
+                } else {
+                    throw new Error('Password must contain at least 1 capital letter.');
+                }
+            } else {
+                throw new Error('Password must contain at least 1 number.');
+            }
+        } else {
+            throw new Error('Password must be at least 8 characters long.'); //eventueel nog else if om te kijken of password kleiner is dan 128 tekens
+        }
     }
 
     equals(otherUser: { id?: number; email: string; password: string }): boolean {
--- back-end/test/domain/model/user.test.ts
+++ back-end/test/domain/model/user.test.ts
@@ -1,14 +1,73 @@
+import { error } from 'console';
 import { User } from '../../../domain/model/user';
+const validEmail = 'test@ucll.be';
+const validPassword = 'Password!123';
 
 test(`given: valid values for User, when: User is created, then: User is created with those values`, () => {
     // given
-    const email = 'test';
-    const password = 'test';
 
     // when
-    const user = new User({ email, password });
+    const user = new User({ email: validEmail, password: validPassword });
 
     // then
-    expect(user.email).toEqual(email);
-    expect(user.password).toEqual(password);
+    expect(user.email).toEqual(validEmail);
+    expect(user.password).toEqual(validPassword);
+});
+
+test(`given: invalid email for User, when: User is created, then: User is not created and an error is thrown`, () => {
+    //given
+    const invalidEmail = 'test';
+
+    //when
+    const createUserInvalidEmail = () => new User({ email: invalidEmail, password: validPassword });
+
+    //then
+    expect(createUserInvalidEmail).toThrow("Email must contain a '@'.");
+});
+
+//tests for password
+
+test(`given: invalid password (too short) for User, when: User is created, then: User is not created and error is thrown`, () => {
+    //given
+    const invalidPassword = 'weakest';
+    //when
+    const createUserInvalidPassword = () =>
+        new User({ email: validEmail, password: invalidPassword });
+    //then
+
+    expect(createUserInvalidPassword).toThrow('Password must be at least 8 characters long.');
+});
+
+test(`given: invalid password (no number) for User,  when: User is created, then: User is not created and error is thrown`, () => {
+    //given
+    const invalidPassword = 'lessbutstillweak';
+    //when
+    const createUserInvalidPassword = () =>
+        new User({ email: validEmail, password: invalidPassword });
+    //then
+
+    expect(createUserInvalidPassword).toThrow('Password must contain at least 1 number.');
+});
+
+test(`given: invalid password (no capital letter) for User,  when: User is created, then: User is not created and error is thrown`, () => {
+    //given
+    const invalidPassword = 'veryclose1';
+    //when
+    const createUserInvalidPassword = () =>
+        new User({ email: validEmail, password: invalidPassword });
+    //then
+
+    expect(createUserInvalidPassword).toThrow('Password must contain at least 1 capital letter.');
+});
+
+test(`given: invalid password (no special sign) for User,  when: User is created, then: User is not created and error is thrown`, () => {
+    //given
+    const invalidPassword = 'Veryveryclose1';
+    //when
+    const createUserInvalidPassword = () =>
+        new User({ email: validEmail, password: invalidPassword });
+    //then
+    expect(createUserInvalidPassword).toThrow(
+        `Password must contain at least 1 special sign (!@#$%^&*()_+-=\[\]{};':"\\|,.<>\/?).`
+    );
 });
