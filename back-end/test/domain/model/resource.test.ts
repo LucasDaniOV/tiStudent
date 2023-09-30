88d10f12c8cc9b28e6f0993fc1be30a39e3f6701
Lucas Oude Vrielink <lucas@oudevrielink.be>
2023-09-30 12:07:50 +0200

    Resource test: change creator to creatorId

--- back-end/test/domain/model/resource.test.ts
+++ back-end/test/domain/model/resource.test.ts
@@ -1,4 +1,3 @@
-import { User } from '../../../domain/model/user';
 import { Resource } from '../../../domain/model/resource';
 import { Category } from '../../../domain/model/category';
 import { Subject } from '../../../domain/model/subject';
@@ -6,17 +5,17 @@ import { Subject } from '../../../domain/model/subject';
 test(`given: valid values for Resource, when: Resource is created, then: Resource is created`, () => {
     // given
     const id = 1;
-    const creator = new User({ email: 'sudo@tistudent.be', password: 'TopSecret007$' });
+    const creatorId = 1;
     const title = 'Hello World';
     const description = 'This is a test resource';
     const category = Category.CheatSheet;
     const subject = Subject.FullStack_Software_Develoment;
 
     // when
-    const resource = new Resource({ id, creator, title, description, category, subject });
+    const resource = new Resource({ id, creatorId, title, description, category, subject });
 
     // then
-    expect(resource.creator).toEqual(creator);
+    expect(resource.creatorId).toEqual(creatorId);
     expect(resource.getTitle()).toEqual(title);
     expect(resource.getDescription()).toEqual(description);
     expect(resource.getCategory()).toEqual(category);
