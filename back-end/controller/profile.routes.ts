39323feec75106bc86ff7f04deeddd32c2ec7664
Lucas Oude Vrielink <lucas@oudevrielink.be>
2023-10-04 19:19:14 +0200

    Swagger: Profile

--- back-end/controller/profile.routes.ts
+++ back-end/controller/profile.routes.ts
@@ -1,9 +1,64 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Profile:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: number
+ *           format: int64
+ *           example: 0
+ *         user:
+ *           ref: '#/components/schemas/User'
+ *         username:
+ *           type: string
+ *           example: 'johndoe'
+ *         bio:
+ *           type: string
+ *           example: 'Software engineer at Google'
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           example: '2021-01-01T00:00:00.000Z'
+ *         latestActivity:
+ *           type: string
+ *           format: date-time
+ *           example: '2023-01-01T00:00:00.000Z'
+ *     ProfileInput:
+ *       type: object
+ *       properties:
+ *         userId:
+ *           type: number
+ *           format: int64
+ *           example: 1
+ *         username:
+ *           type: string
+ *           example: 'johndoe'
+ */
 import express, { Request, Response } from 'express';
 import profileService from '../service/profile.service';
 import { ProfileInput } from '../types';
 
 const profileRouter = express.Router();
 
+/**
+ * @swagger
+ * /profiles:
+ *   get:
+ *     tags:
+ *       - profiles
+ *     summary: Get all profiles
+ *     responses:
+ *       200:
+ *         description: List of profiles
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Profile' 
+ */
 profileRouter.get('/', (req: Request, res: Response) => {
     try {
         const profiles = profileService.getAllProfiles();
@@ -13,6 +68,29 @@ profileRouter.get('/', (req: Request, res: Response) => {
     }
 });
 
+/**
+ * @swagger
+ * /profiles/{id}:
+ *   get:
+ *     tags:
+ *       - profiles
+ *     summary: Get a profile by id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *           format: int64
+ *           required: true
+ *           description: The profile id
+ *     responses:
+ *       200:
+ *         description: A profile object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Profile'
+ */
 profileRouter.get('/:id', (req: Request, res: Response) => {
     try {
         const id = parseInt(req.params.id);
@@ -23,6 +101,28 @@ profileRouter.get('/:id', (req: Request, res: Response) => {
     }
 });
 
+/**
+ * @swagger
+ * /profiles:
+ *   post:
+ *     tags:
+ *       - profiles
+ *     summary: Create a profile
+ *     requestBody:
+ *       description: Profile object to be created
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ProfileInput'
+ *     responses:
+ *       200:
+ *         description: The created profile
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Profile'
+ */
 profileRouter.post('/', (req: Request, res: Response) => {
     try {
         const profileInput = req.body as ProfileInput;
