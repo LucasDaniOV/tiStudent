75521bea884383c7cc0ae752c64a1cf693318efa
DavyBellens <r0882857@ucll.be>
2023-10-04 17:32:57 +0200

    Swagger: User

--- back-end/controller/user.routes.ts
+++ back-end/controller/user.routes.ts
@@ -1,8 +1,60 @@
+/**
+ * @swagger
+ *   components:
+ *     schemas:
+ *       User:
+ *         type: object
+ *         properties:
+ *           id:
+ *             type: number
+ *             format: int64
+ *           email:
+ *             type: string
+ *             format: email
+ *           password:
+ *             type: string
+ *             format: password
+ *       UserInput:
+ *         type: object
+ *         properties:
+ *           id:
+ *             type: number
+ *             format: int64
+ *           email:
+ *             type: string
+ *             format: email
+ *           password:
+ *             type: string
+ *             format: password
+ */
+
 import express, { Request, Response } from 'express';
 import userService from '../service/user.service';
 import { UserInput } from '../types';
 
 const userRouter = express.Router();
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     tags:
+ *       - users
+ *     summary: Add a new User
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UserInput'
+ *     responses:
+ *       '200':
+ *         description: Information about the new user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ */
 
 userRouter.post('/', (req: Request, res: Response) => {
     try {
@@ -14,6 +66,21 @@ userRouter.post('/', (req: Request, res: Response) => {
     }
 });
 
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     tags:
+ *       - users
+ *     summary: Returns a list of all the users
+ *     responses:
+ *       '200':
+ *         description: List of all users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ */
 userRouter.get('/', (req: Request, res: Response) => {
     try {
         const users = userService.getAllUsers();
