f58f81086fc9623efc591c004d54a7d360b4201a
Lucas Oude Vrielink <lucas@oudevrielink.be>
2023-10-19 13:16:54 +0200

    prisma setup WIP

--- back-end/domain/model/user.ts
+++ back-end/domain/model/user.ts
@@ -1,11 +1,12 @@
+import { User as UserPrisma } from '@prisma/client';
 export class User {
     readonly id?: number;
     private _email: string;
     private _password: string;
 
     constructor(user: { id?: number; email: string; password: string }) {
-        this.validateEmail(user.email);
-        this.validatePassword(user.password);
+        User.validateEmail(user.email);
+        User.validatePassword(user.password);
 
         this.id = user.id;
         this._email = user.email;
@@ -16,11 +17,11 @@ export class User {
         return this.id == otherUser.id && this.email === otherUser.email && this._password === otherUser.password;
     }
 
-    validateEmail = (email: string): void => {
+    public static validateEmail = (email: string): void => {
         if (!email.includes('@')) throw new Error("email must contain a '@'");
     };
 
-    validatePassword = (password: string): void => {
+    public static validatePassword = (password: string): void => {
         if (password.length < 8) throw new Error('password must be at least 8 characters long');
         if (!password.match(/\d/)) throw new Error('password must contain at least 1 number');
         if (!password.match(/[A-Z]/)) throw new Error('password must contain at least 1 capital letter');
@@ -38,14 +39,26 @@ export class User {
     }
 
     public set email(v: string) {
-        this.validateEmail(v);
+        User.validateEmail(v);
         if (v == this._email) throw new Error(`new email must be different from old email`);
         this._email = v;
     }
 
     public set password(v: string) {
-        this.validatePassword(v);
+        User.validatePassword(v);
         if (v == this._password) throw new Error(`new password must be different from old password`);
         this._password = v;
+    }
+
+    public static from({
+        id,
+        email,
+        password,
+    }: UserPrisma): User {
+        return new User({
+            id: id,
+            email: email,
+            password: password,
+        })
     }
 }
