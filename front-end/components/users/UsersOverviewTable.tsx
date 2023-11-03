c74f0c08183e6f2ae5e4b1eeff02f4d62ac8ea97
Lucas Oude Vrielink <lucas@oudevrielink.be>
2023-11-03 22:10:39 +0100

    user frontend: delete functionality

--- front-end/components/users/UsersOverviewTable.tsx
+++ front-end/components/users/UsersOverviewTable.tsx
@@ -23,9 +23,6 @@ const UsersOverviewTable: React.FC<Props> = ({ users }: Props) => {
             {users.map((user, index) => (
               <tr
                 key={index}
-                onDoubleClick={() => {
-                  router.push("/users/delete/" + user.id);
-                }}
                 onClick={() => {
                   router.push("/users/" + user.id);
                 }}
@@ -33,6 +30,14 @@ const UsersOverviewTable: React.FC<Props> = ({ users }: Props) => {
                 <td>{user.id}</td>
                 <td>{user.email}</td>
                 <td>{user.password}</td>
+                <td
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    if (confirm(`Are you sure you want to delete ${user.id}?`)) router.push("/users/delete/" + user.id);
+                  }}
+                >
+                  delete
+                </td>
               </tr>
             ))}
           </tbody>
