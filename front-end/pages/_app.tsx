7bf3f3473c190c04e967389740886343ef8e7e63
Lucas Oude Vrielink <lucas@oudevrielink.be>
2023-11-07 09:33:23 +0100

    disable keycloak login

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,9 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-import KeycloakLoginRequired from '@/hooks/KeycloakLoginRequired';
-
 export default function App({ Component, pageProps }: AppProps) {
-  const isLogin = KeycloakLoginRequired();
-  return isLogin ? <Component {...pageProps} /> : <div>Login required</div>;
+  return <Component {...pageProps} />;
 }
