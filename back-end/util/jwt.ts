fa0f2c761ef5e1716d229b74bb0418f6e2a3262e
Lucas Oude Vrielink <lucas@oudevrielink.be>
2023-11-22 11:02:07 +0100

    generateJwtToken file

---
+++ back-end/util/jwt.ts
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+
+const generateJwtToken = ({ email }): string => {
+    const options = { expiresIn: `${process.env.JWT_EXPIRES_HOURS}h`, issuer: process.env.JWT_ISSUER };
+    const secret = process.env.JWT_SECRET;
+
+    try {
+        return jwt.sign({ email }, secret, options);
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error generating JWT token, see server log for details.');
+    }
+};
+
+export { generateJwtToken };
