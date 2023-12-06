8283f1cba6ae9ceaf5ae651f0b5a2d2356659cee
DavyBellens <r0882857@ucll.be>
2023-12-06 18:40:57 +0100

    some styling

--- front-end/components/users/GithubLoginButton.tsx
+++ front-end/components/users/GithubLoginButton.tsx
@@ -1,7 +1,14 @@
 const GithubLoginButton: React.FC = () => {
-    const clientId = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
-    const authUrl = `https://github.com/login/oauth/authorize?scope=read:user&client_id=${clientId}`;
-    return <a href={authUrl}>Sign in with GitHub</a>;
-}
+  const clientId = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
+  const authUrl = `https://github.com/login/oauth/authorize?scope=read:user&client_id=${clientId}`;
+  return (
+    <a
+      href={authUrl}
+      className="bg-gray-500 hover:bg-gray-300 hover:text-black p-1"
+    >
+      Sign in with GitHub
+    </a>
+  );
+};
 
 export default GithubLoginButton;
