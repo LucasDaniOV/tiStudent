2f19050f8fb6e183adac9a91b04417bcc92d9ff5
DavyBellens <r0882857@ucll.be>
2023-12-14 21:06:34 +0100

    profiles i18n

--- front-end/pages/profiles/index.tsx
+++ front-end/pages/profiles/index.tsx
@@ -1,3 +1,4 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Header from "@/components/header";
 import ProfilesOverviewTable from "@/components/profiles/ProfilesOverviewTable";
 import { Profile } from "@/types";
@@ -6,11 +7,12 @@ import { useEffect, useState } from "react";
 import { mutate } from "swr";
 import useInterval from "use-interval";
 import ProfileService from "../../services/ProfileService";
+import { useTranslation } from "next-i18next";
 
 const Profiles: React.FC = () => {
   const [profiles, setProfiles] = useState<Array<Profile>>();
   const [authorized, setAuthorized] = useState<boolean>(false);
-
+  const { t } = useTranslation();
   const getProfiles = async () => {
     const response = await ProfileService.getAllProfiles();
     if (response.status === "unauthorized" || response.status === "error") {
@@ -32,21 +34,30 @@ const Profiles: React.FC = () => {
   return (
     <>
       <Head>
-        <title>Profiles</title>
+        <title>{t("profiles.index.title")}</title>
       </Head>
       <Header current="profiles" />
       <main>
-        <h1>Profiles</h1>
+        <h1>{t("profiles.index.title")}</h1>
         <section>
           {authorized ? (
             profiles && <ProfilesOverviewTable profiles={profiles} />
           ) : (
-            <p>You are not authorized to view this page. Please login first.</p>
+            <p>{t("authorization.error")}</p>
           )}
         </section>
       </main>
     </>
   );
+};
+
+export const getServerSideProps = async (context: any) => {
+  const { locale } = context;
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? "en", ["common"])),
+    },
+  };
 };
 
 export default Profiles;
