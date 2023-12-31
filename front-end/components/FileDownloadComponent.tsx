c915af07bcf23f3a10a450d145e5a78d903a3cae
DavyBellens <r0882857@ucll.be>
2023-12-31 01:11:30 +0100

    upload and download components

---
+++ front-end/components/FileDownloadComponent.tsx
@@ -0,0 +1,76 @@
+import { getToken } from "@/util/token";
+import { useRouter } from "next/router";
+import React, { useState, ChangeEvent, useEffect } from "react";
+
+type Props = {
+  fileName: any;
+};
+
+const FileUploadComponent: React.FC<Props> = ({ fileName }: Props) => {
+  const [uploadedFile, setUploadedFile] = useState<any>();
+  useEffect(() => {
+    setUploadedFile(fileName);
+  }, [uploadedFile]);
+  console.log(uploadedFile);
+
+  const handleDownload = async () => {
+    if (uploadedFile) {
+      const token = getToken();
+      const url = `${process.env.NEXT_PUBLIC_API_URL}/files/${uploadedFile}`;
+
+      try {
+        // Use fetch for client-side navigation with headers
+        const response = await fetch(url, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.ok) {
+          // Convert the response to a Blob
+          const blob = await response.blob();
+
+          // Create a temporary URL for the Blob
+          const blobUrl = URL.createObjectURL(blob);
+
+          // Create an <a> element to trigger the download
+          const downloadLink = document.createElement("a");
+          downloadLink.href = blobUrl;
+          downloadLink.download = uploadedFile;
+
+          // Append the <a> element to the DOM and trigger the download
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+          document.body.removeChild(downloadLink);
+
+          // Clean up the temporary URL
+          URL.revokeObjectURL(blobUrl);
+        } else {
+          // Handle download failure
+          console.error("Failed to download file.");
+        }
+      } catch (error) {
+        // Handle fetch error
+        console.error("Error during fetch:", error);
+      }
+    }
+  };
+
+  return (
+    <div className="m-2 p-1">
+      {uploadedFile && (
+        <div className="">
+          <button
+            className="text-2xl align-middle hover:bg-gray-500"
+            onClick={handleDownload}
+          >
+            <span className="text-3xl align-middle">&#10515;</span> Download
+            File
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileUploadComponent;
--- front-end/components/FileUploadComponent.tsx
+++ front-end/components/FileUploadComponent.tsx
@@ -2,10 +2,12 @@ import { getToken } from "@/util/token";
 import { useRouter } from "next/router";
 import React, { useState, ChangeEvent } from "react";
 
-const FileUploadComponent: React.FC = () => {
+type Props = {
+  callback: Function;
+};
+
+const FileUploadComponent: React.FC<Props> = ({ callback }: Props) => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const [uploadedFile, setUploadedFile] = useState<any>(null);
-  const router = useRouter();
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     if (files && files.length > 0) {
@@ -13,24 +15,31 @@ const FileUploadComponent: React.FC = () => {
     }
   };
 
-  const handleUpload = async () => {
+  const handleUpload = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    e.stopPropagation();
     if (selectedFile) {
       try {
         const formData = new FormData();
         formData.append("file", selectedFile);
         const token = getToken();
         // Send the file to the server
-        const response = await fetch("http://localhost:3000/files", {
-          method: "POST",
-          body: formData,
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        const response = await fetch(
+          process.env.NEXT_PUBLIC_API_URL + "/files",
+          {
+            method: "POST",
+            body: formData,
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
 
         if (response.ok) {
           const result = await response.json();
-          setUploadedFile(result.file);
+          callback(result.file.filename);
           console.log("File uploaded successfully!");
         } else {
           console.error("Failed to upload file.");
@@ -43,61 +52,10 @@ const FileUploadComponent: React.FC = () => {
     }
   };
 
-  const handleDownload = async () => {
-    if (uploadedFile) {
-      const token = getToken();
-      const url = `http://localhost:3000/files/${uploadedFile.filename}`;
-
-      try {
-        // Use fetch for client-side navigation with headers
-        const response = await fetch(url, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        if (response.ok) {
-          // Convert the response to a Blob
-          const blob = await response.blob();
-
-          // Create a temporary URL for the Blob
-          const blobUrl = URL.createObjectURL(blob);
-
-          // Create an <a> element to trigger the download
-          const downloadLink = document.createElement("a");
-          downloadLink.href = blobUrl;
-          downloadLink.download = uploadedFile.filename;
-
-          // Append the <a> element to the DOM and trigger the download
-          document.body.appendChild(downloadLink);
-          downloadLink.click();
-          document.body.removeChild(downloadLink);
-
-          // Clean up the temporary URL
-          URL.revokeObjectURL(blobUrl);
-        } else {
-          // Handle download failure
-          console.error("Failed to download file.");
-        }
-      } catch (error) {
-        // Handle fetch error
-        console.error("Error during fetch:", error);
-      }
-    }
-  };
-
   return (
-    <div>
+    <div className="m-2 p-1">
       <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
-
-      {uploadedFile && (
-        <div>
-          <h2>Uploaded File Details</h2>
-          <pre>{JSON.stringify(uploadedFile, null, 2)}</pre>
-          <button onClick={handleDownload}>Download File</button>
-        </div>
-      )}
+      <button onClick={(e) => handleUpload(e)}>Upload</button>
     </div>
   );
 };
