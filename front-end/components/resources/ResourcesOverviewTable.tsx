5380808d8e6af7058763fd440033771f3e0925a1
DavyBellens <r0882857@ucll.be>
2024-01-03 04:35:42 +0100

    Figured out individual and unique thumbnails

--- front-end/components/resources/ResourcesOverviewTable.tsx
+++ front-end/components/resources/ResourcesOverviewTable.tsx
@@ -4,23 +4,39 @@ import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import React, { MouseEvent, useEffect, useState } from "react";
 import { Profile, Resource } from "../../types";
+import Image from "next/image";
 
 type Props = {
   resources: any[];
 };
+
+type imageMap = Record<string, string>;
 const ResourceOverviewTable: React.FC<Props> = ({ resources }: Props) => {
   const [profile, setProfile] = useState<Profile>();
+  const [imageState, setImageState] = useState<imageMap>({});
   const router = useRouter();
   const { t } = useTranslation();
+  const images: imageMap = {};
+
+  const mapImgToResource = async () => {
+    await Promise.all(
+      resources.map(async (r: Resource) => {
+        const img = await import("../../../uploads/" + r.thumbNail);
+        images[r.id] = img;
+      })
+    );
+    setImageState(images);
+  };
+
   useEffect(() => {
     const getProfile = async () => {
       const p = String(sessionStorage.getItem("loggedInUser"));
       if (!p) return;
       const pObject = await ProfileService.getProfileById(JSON.parse(p).id);
-      console.log(pObject.data);
-      if (pObject) setProfile(pObject.data);
+      if (pObject) setProfile(pObject.profile);
     };
     getProfile();
+    mapImgToResource();
   }, []);
 
   const deleteResource = async (resource: Resource) => {
@@ -39,6 +55,7 @@ const ResourceOverviewTable: React.FC<Props> = ({ resources }: Props) => {
   const checkAuthority = async (e: MouseEvent, resource: Resource) => {
     e.stopPropagation();
     e.preventDefault();
+
     if (profile?.role === "ADMIN") {
       deleteResource(resource);
     } else {
@@ -61,6 +78,9 @@ const ResourceOverviewTable: React.FC<Props> = ({ resources }: Props) => {
               </th>
               <th scope="col" className="border p-4 text-left">
                 {t("resources.fields.user.id")}
+              </th>
+              <th scope="col" className="border p-4">
+                Thumbnail
               </th>
               <th scope="col" className="border p-4 text-left">
                 {t("resources.fields.created.at")}
@@ -93,6 +113,16 @@ const ResourceOverviewTable: React.FC<Props> = ({ resources }: Props) => {
               >
                 <td className="border p-4">{resource.id}</td>
                 <td className="border p-4">{resource.profileId}</td>
+                <td>
+                  <span className="flex items-center justify-center">
+                    <Image
+                      src={imageState[resource.id]}
+                      alt={"Thumbnail"}
+                      width={100}
+                      height={50}
+                    />
+                  </span>
+                </td>
                 <td className="border p-4">{String(resource.createdAt)}</td>
                 <td className="border p-4">{resource.title}</td>
                 <td className="border p-4">{resource.description}</td>
