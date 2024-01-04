ee6c455fcb174aa265709becc2da6d74272918b0
Lucas Oude Vrielink <lucas@oudevrielink.be>
2024-01-04 14:15:25 +0100

    validation for id in Category model

--- back-end/domain/model/category.ts
+++ back-end/domain/model/category.ts
@@ -5,7 +5,7 @@ export class Category {
     readonly name: string;
 
     constructor(category: { id: number; name: string }) {
-        Category.validate(category.name);
+        Category.validate(category.name, category.id);
         this.id = category.id;
         this.name = category.name;
     }
@@ -14,12 +14,20 @@ export class Category {
         return this.id === otherCategory.id && this.name === otherCategory.name;
     }
 
-    static validate(name: string): void {
+    static validate(name: string, id: number): void {
         Category.validateName(name);
+        Category.validateId(id);
     }
+
+    static validateId = (id: number) => {
+        if (!id) throw new Error('id is required');
+        if (typeof id !== 'number') throw new Error('id must be a number');
+        if (id < 0) throw new Error('id cannot be negative');
+    };
 
     static validateName = (name: string) => {
         if (!name) throw new Error('name is required');
+        if (typeof name !== 'string') throw new Error('name must be a string');
         if (name.length > 60) throw new Error('name cannot be longer than 60 characters');
     };
 
