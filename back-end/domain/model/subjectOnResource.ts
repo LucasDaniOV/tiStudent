ed3fa876a5f1f814e2236bf22c0ab49d550c01a3
Lucas Oude Vrielink <lucas@oudevrielink.be>
2024-01-04 14:23:51 +0100

    add validation for subjectId and resourceId in SubjectOnResource class

--- back-end/domain/model/subjectOnResource.ts
+++ back-end/domain/model/subjectOnResource.ts
@@ -5,9 +5,27 @@ export class SubjectOnResource {
     readonly resourceId: number;
 
     constructor(subjectId: number, resourceId: number) {
+        SubjectOnResource.validate(subjectId, resourceId);
         this.subjectId = subjectId;
         this.resourceId = resourceId;
     }
+
+    static validate(subjectId: number, resourceId: number): void {
+        SubjectOnResource.validateSubjectId(subjectId);
+        SubjectOnResource.validateResourceId(resourceId);
+    }
+
+    static validateSubjectId = (subjectId: number) => {
+        if (!subjectId) throw new Error('subjectId is required');
+        if (subjectId < 0) throw new Error('subjectId cannot be negative');
+        if (typeof subjectId !== 'number') throw new Error('subjectId must be a number');
+    };
+
+    static validateResourceId = (resourceId: number) => {
+        if (!resourceId) throw new Error('resourceId is required');
+        if (resourceId < 0) throw new Error('resourceId cannot be negative');
+        if (typeof resourceId !== 'number') throw new Error('resourceId must be a number');
+    };
 
     equals(otherSubjectOnResource: SubjectOnResource): boolean {
         return (
