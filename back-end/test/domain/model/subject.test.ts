5d93e445d3c3a0b9bde6f337ec9b7492568c5675
Lucas Oude Vrielink <lucas@oudevrielink.be>
2024-01-04 14:21:40 +0100

    add tests for subject id validation

--- back-end/test/domain/model/subject.test.ts
+++ back-end/test/domain/model/subject.test.ts
@@ -35,6 +35,57 @@ test(`given: too long name for subject, when: creating a subject, then: should t
     expect(createSubject).toThrowError('name cannot be longer than 60 characters');
 });
 
+test(`given: not a string for subject, when: creating a subject, then: should throw an error`, () => {
+    // given
+    const invalidName = 1;
+
+    // when
+    const createSubject = () => new Subject({ id, name: invalidName as unknown as string });
+
+    // then
+    expect(createSubject).toThrowError('name must be a string');
+});
+
+test(`given: null for subject id, when: creating a subject, then: should throw an error`, () => {
+    // given
+    // when
+    const createSubject = () => new Subject({ id: null, name });
+
+    // then
+    expect(createSubject).toThrowError('id is required');
+});
+
+test(`given: undefined for subject id, when: creating a subject, then: should throw an error`, () => {
+    // given
+    // when
+    const createSubject = () => new Subject({ id: undefined, name });
+
+    // then
+    expect(createSubject).toThrowError('id is required');
+});
+
+test(`given: not a number for subject id, when: creating a subject, then: should throw an error`, () => {
+    // given
+    const invalidId = '1';
+
+    // when
+    const createSubject = () => new Subject({ id: invalidId as unknown as number, name });
+
+    // then
+    expect(createSubject).toThrowError('id must be a number');
+});
+
+test(`given: negative number for subject id, when: creating a subject, then: should throw an error`, () => {
+    // given
+    const invalidId = -1;
+
+    // when
+    const createSubject = () => new Subject({ id: invalidId, name });
+
+    // then
+    expect(createSubject).toThrowError('id cannot be negative');
+});
+
 test(`given: valid values for subject, when: comparing two subjects, then: should return true`, () => {
     // given
     const subject = new Subject({ id, name });
