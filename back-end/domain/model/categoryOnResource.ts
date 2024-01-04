51dbbdf99fedf030fcda06e89e874f6093e2f071
Lucas Oude Vrielink <lucas@oudevrielink.be>
2024-01-04 14:14:04 +0100

    add validation for categoryId and resourceId in CategoryOnResource constructor

--- back-end/domain/model/categoryOnResource.ts
+++ back-end/domain/model/categoryOnResource.ts
@@ -5,9 +5,27 @@ export class CategoryOnResource {
     readonly resourceId: number;
 
     constructor(categoryId: number, resourceId: number) {
+        CategoryOnResource.validate(categoryId, resourceId);
         this.categoryId = categoryId;
         this.resourceId = resourceId;
     }
+
+    static validate(categoryId: number, resourceId: number): void {
+        CategoryOnResource.validateCategoryId(categoryId);
+        CategoryOnResource.validateResourceId(resourceId);
+    }
+
+    static validateCategoryId = (categoryId: number) => {
+        if (!categoryId) throw new Error('categoryId is required');
+        if (categoryId < 0) throw new Error('categoryId cannot be negative');
+        if (typeof categoryId !== 'number') throw new Error('categoryId must be a number');
+    };
+
+    static validateResourceId = (resourceId: number) => {
+        if (!resourceId) throw new Error('resourceId is required');
+        if (resourceId < 0) throw new Error('resourceId cannot be negative');
+        if (typeof resourceId !== 'number') throw new Error('resourceId must be a number');
+    };
 
     equals(otherCategoryOnResource: CategoryOnResource): boolean {
         return (
