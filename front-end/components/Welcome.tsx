d350effa254b9499cc59cf3b112af7ee016c88bf
DavyBellens <r0882857@ucll.be>
2024-01-06 03:03:55 +0100

    welcome message component

---
+++ front-end/components/Welcome.tsx
@@ -0,0 +1,27 @@
+import { useTranslation } from "next-i18next";
+import Image from "next/image";
+
+type Props = {
+  name: string;
+};
+
+const Welcome: React.FC<Props> = ({ name }: Props) => {
+  const { t } = useTranslation();
+  return (
+    <div className="p-20">
+      <h1 className="text-3xl font-bold">
+        {t("home.welcome")} {name}!
+      </h1>
+      <Image
+        src="/images/einstein.jpg"
+        alt="einstein smoking a cigar while browsing our website"
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: "auto", height: "auto" }}
+      />
+    </div>
+  );
+};
+
+export default Welcome;
